a17cfb6912a1873d1086c7d3c79ebf23cea8d474
Lawn Meower <s_schmidt1993@outlook.de>
2025-01-26 10:35:01 +0100

    Added EntityGroup class

---
+++ EntityGroup.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexLoom
+{
+    public partial class EntityGroup : ContentView
+    {
+        public EntityGroup()
+        {
+            InitializeComponent();
+        }
+
+        private void onAddItemClicked(object sender, EventArgs e)
+        {
+            EntityStack.Children.Add(new Entity());
+        }
+
+        private void onPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    this.Opacity = 0.5;
+                    break;
+                case GestureStatus.Running:
+                    this.TranslationY = e.TotalY;
+                    break;
+                case GestureStatus.Completed:
+                {
+                    this.Opacity = 1;
+                    this.TranslationY = 0;
+                    var parentStack = this.Parent as VerticalStackLayout;
+
+                    if (parentStack != null)
+                    {
+                        var items = parentStack.Children;
+                        int currentIndex = items.IndexOf(this);
+                        int newIndex = (int)(e.TotalY / this.Height);
+                        newIndex = Math.Clamp(newIndex, 0, items.Count - 1);
+
+                        if (newIndex != currentIndex)
+                        {
+                            items.RemoveAt(currentIndex);
+                            items.Insert(newIndex, this);
+                        }
+                    }
+                } break;
+            }
+        }
+
+        private void onDeleteClicked(object sender, EventArgs e)
+        {
+            var parentStack = this.Parent as VerticalStackLayout;
+
+            if (parentStack != null)
+                parentStack.Children.Remove(this);
+        }
+    }
+}
