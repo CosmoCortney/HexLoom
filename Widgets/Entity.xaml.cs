60924de1f0039761f614308439a30e9b5ac655b3
Lawn Meower <s_schmidt1993@outlook.de>
2025-03-03 22:41:36 +0100

    implemented PanUpdate helper functions

--- Widgets/Entity.xaml.cs
+++ Widgets/Entity.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -150,35 +150,7 @@ namespace HexLoom
 
         private void onPanUpdated(object sender, PanUpdatedEventArgs e)
         {
-            switch (e.StatusType)
-            {
-                case GestureStatus.Started:
-                    this.Opacity = 0.5;
-                    break;
-                case GestureStatus.Running:
-                    this.TranslationY = e.TotalY;
-                    break;
-                case GestureStatus.Completed:
-                {
-                    this.Opacity = 1;
-                    this.TranslationY = 0;
-                    var parentStack = this.Parent as VerticalStackLayout;
-
-                    if (parentStack != null)
-                    {
-                        var items = parentStack.Children;
-                        int currentIndex = items.IndexOf(this);
-                        int newIndex = (int)(e.TotalY / this.Height);
-                        newIndex = Math.Clamp(newIndex, 0, items.Count - 1);
-
-                        if (newIndex != currentIndex)
-                        {
-                            items.RemoveAt(currentIndex);
-                            items.Insert(newIndex, this);
-                        }
-                    }
-                } break;
-            }
+            Helpers.PanUpdated(this, sender, e);
         }
 
         private void onDeleteClicked(object sender, EventArgs e)
