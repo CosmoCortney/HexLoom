4536e35e10c66ce5005a7f87feb8416d25f5154d
Lawn Meower <s_schmidt1993@outlook.de>
2025-03-03 22:38:56 +0100

    added widget padding setter function

--- Helpers/Helpers.cs
+++ Helpers/Helpers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -53,6 +53,52 @@ namespace HexLoom
                 ++length;
 
             return length;
+        }
+
+        public static void SetWidgetPadding<Widget>(Int32 topLeft, Int32 topRight, Int32 bottomLeft, Int32 bottomRight)
+        {
+            if (typeof(Widget) == typeof(Entry))
+            {
+                Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("", (handler, view) =>
+                {
+                    if (view is Entry)
+                        handler.PlatformView.Padding = new Microsoft.UI.Xaml.Thickness(topLeft, topRight, bottomLeft, bottomRight);
+                });
+            }
+            else if (typeof(Widget) == typeof(Button))
+            {
+                Microsoft.Maui.Handlers.ButtonHandler.Mapper.AppendToMapping("", (handler, view) =>
+                {
+                    if (view is Microsoft.Maui.Controls.Button)
+                    {
+                        handler.PlatformView.Padding = new Microsoft.UI.Xaml.Thickness(topLeft, topRight, bottomLeft, bottomRight);
+                        handler.PlatformView.Margin = new Microsoft.UI.Xaml.Thickness(topLeft, topRight, bottomLeft, bottomRight);
+                        handler.PlatformView.HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Left;
+                    }
+                });
+            }
+            else if (typeof(Widget) == typeof(Picker))
+            {
+                Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping("", (handler, view) =>
+                {
+                    if (view is Picker)
+                        handler.PlatformView.Padding = new Microsoft.UI.Xaml.Thickness(topLeft, topRight, bottomLeft, bottomRight);
+                });
+            }
+            else if(typeof(Widget) == typeof(CheckBox))
+            {
+                Microsoft.Maui.Handlers.CheckBoxHandler.Mapper.AppendToMapping("", (handler, view) =>
+                {
+                    if (view is CheckBox)
+                    {
+                        handler.PlatformView.Content = null;
+                        handler.PlatformView.HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Center;
+                        handler.PlatformView.VerticalAlignment = Microsoft.UI.Xaml.VerticalAlignment.Center;
+                        handler.PlatformView.Padding = new Microsoft.UI.Xaml.Thickness(0, 0, 0, 0);
+                        handler.PlatformView.Margin = new Microsoft.UI.Xaml.Thickness(0, 0, 0, 0);
+                    }
+                });
+            }
         }
 
         public static byte[] ByteSwap(byte[] input)
