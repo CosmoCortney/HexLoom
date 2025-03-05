40cafd868584488b1067483cf8f46648eff1d0ab
Lawn Meower <s_schmidt1993@outlook.de>
2025-03-05 11:12:39 +0100

    Maximize on startup

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace HexLoom
+﻿using System.Runtime.InteropServices;
+
+namespace HexLoom
 {
     public partial class App : Application
     {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, Int32 cmdShow);
+
         public App()
         {
             InitializeComponent();
+
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+                var nativeWindow = handler.PlatformView;
+                nativeWindow.Activate();
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                ShowWindow(windowHandle, 3);
+            });
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
